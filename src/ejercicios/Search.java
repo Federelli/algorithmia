f8d98d07b8c6f57afc5645e12b767bdea6b0eee2
Federelli <federelli@gmail.com>
2017-07-17 01:22:45 -0300

    O(n) para buscar suma

--- src/ejercicios/Search.java
+++ src/ejercicios/Search.java
@@ -2,12 +2,16 @@ package ejercicios;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 public class Search {
 
 	@Test
 	public void findSortedNotUsingLength() {
+		//O(n)
 		int number = 5;
 		Integer[] nums = { 1, 5, 9, 12, 21 };
 		int index = 0;
@@ -23,7 +27,8 @@ public class Search {
 	}
 	
 	@Test
-	public void findIfSumOfTwoExist() {
+	public void findIfSumOfTwoExistsBrute() {
+		//O(n^2)
 		int number = 5;
 		Integer[] nums = {8, 6, 3, 7, 2};
 		boolean sumUp = false;
@@ -33,6 +38,25 @@ public class Search {
 					sumUp = true;
 					return;
 				}
+			}
+		}
+		assertTrue(sumUp);
+	}
+	
+	@Test
+	public void findIfSumOfTwoExistsHash() {
+		//O(n)
+		int number = 5;
+		Integer[] nums = {8, 6, 3, 7, 2};
+		boolean sumUp = false;
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length ;i++) {
+			map.put(nums[i], i);
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if(map.containsKey(number - nums[i])) {
+				sumUp = true;
+				return;
 			}
 		}
 		assertTrue(sumUp);
